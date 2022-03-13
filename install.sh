381b507a0a31d765302eabd48e8d820e56bc3840
piotr <nwg.piotr@gmail.com>
2022-03-13 14:55:27 +0100

    fix path

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 python3 setup.py install --optimize=1
-cp azote-palettes.svg /usr/share/pixmaps/
-cp azote-palettes.desktop /usr/share/applications/
+cp dist/azote-palettes.svg /usr/share/pixmaps/
+cp dist/azote-palettes.desktop /usr/share/applications/

19f4b043c89173eae17b0f5434e5356fed442fa7
piotr <nwg.piotr@gmail.com>
2022-03-13 14:51:57 +0100

    fix import

--- azote_palettes/main.py
+++ azote_palettes/main.py
@@ -22,7 +22,7 @@ from PIL import Image
 from colorthief import ColorThief
 from color_tools import get_colour_name, hex_to_rgb, rgb_to_cmyk
 
-from azote_palettes.common import *
+import azote_palettes.common as common
 
 tempdir = '/tmp' if platform.system() == 'Darwin' else tempfile.gettempdir()
 clipboard_file = os.path.join(tempdir, 'azote-clipboard.png')

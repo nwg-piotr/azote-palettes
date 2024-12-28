2576489fe81598260a4533dd5ff68d614f2e1f10
piotr <nwg.piotr@gmail.com>
2024-12-28 20:38:15 +0100

    fix install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,28 @@
 #!/usr/bin/env bash
 
-python3 setup.py install --optimize=1
-cp dist/azote-palettes.svg /usr/share/pixmaps/
-cp dist/azote-palettes.desktop /usr/share/applications/
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
+  exit
+fi
+
+prefix=
+opts=()
+
+while true; do
+  case "$1" in
+    --prefix=*)
+      opts+=("$1")
+      eval "prefix=${1##--prefix=}"
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+
+opts+=("$@")
+
+: ${prefix:="/usr"}
+
+python3 setup.py install --optimize=1 "${opts[@]}"
